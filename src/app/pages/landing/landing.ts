9a6dc8b56a7ba366048c960fcae46802d87aa510
DiegoAguilar <diegarmand.dev@gmail.com>
2026-08-12 23:59:15 -0600

    feat: hero & bg

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
 import { Footer } from "../../components/footer/footer";
+import { Hero } from "../../components/hero/hero";
 
 @Component({
   selector: 'app-landing',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Hero],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
